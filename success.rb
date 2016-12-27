7c6b711c688139483dbe32966842bb4da4779ace
divya <sjdivya03@gmail.com>
2016-12-27 16:20:51 +0530

    success number new

--- success.rb
+++ success.rb
@@ -1,4 +1,4 @@
 
 a="success in the town of error in the SUCCESS sucCess displaying wrong Form of SuccESS deciding SuccEss"
 number=a.scan(/success/).length
-puts "#{number}"
+puts "Number of times 'success' appearing in the sting is #{number}"
