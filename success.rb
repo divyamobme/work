2b696c48ede1156c5a0d9772b37ba67542226622
divya <sjdivya03@gmail.com>
2016-12-28 12:55:42 +0530

    count success

--- success.rb
+++ success.rb
@@ -1,4 +1,10 @@
-
+class Success
+	def count
 a="success in the town of error in the SUCCESS sucCess displaying wrong Form of SuccESS deciding SuccEss"
+puts "String is \n #{a}"
 number=a.scan(/success/).length
 puts "Number of times 'success' appeared in the sting is #{number}"
+end
+end
+s=Success.new
+s.count
