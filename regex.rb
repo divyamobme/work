4736296574256b0ab4693c47d52ffb39517e71be
divya <sjdivya03@gmail.com>
2016-12-28 12:51:29 +0530

    corrected

--- regex.rb
+++ regex.rb
@@ -1,7 +1,13 @@
-
+class Find
+	def get
 a='hello my dear friends my \transaction_id=\123\ '
-p= a.scan(/[0-9]*/)
+p= a.scan(/\d+/)
+
 
 puts "Transaction_id is "
 puts p
+end
+end
+f=Find.new
+f.get
 
