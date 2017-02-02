08e6dec6177052e09d0787a261c9cf71528b22ca
divyamobme <sjdivya03@gmail.com>
2017-02-02 17:54:56 +0530

    Sinatra blog

---
+++ test_files/app.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+
+
+set :database, "sqlite3:myblog.sqlite3"
+
+
+ require "./models"
+
+# index posts
+get '/' do
+	@posts = Post.all
+	erb :index
+end
+
+# create post
+post '/post' do
+	@post = Post.create(title: params[:title], body: params[:body])
+	redirect '/'
+end
+
+# show post
+get '/post/:id' do
+	@post = Post.find(params[:id])
+	erb :post_page
+end
+
+# update post
+put '/post/:id' do
+	@post = Post.find(params[:id])
+	@post.update(title: params[:title], body: params[:body])
+	@post.save
+	redirect '/post/'+params[:id]
+end
+
+# delete post
+delete '/post/:id' do
+	@post = Post.find(params[:id])
+	@post.destroy
+	redirect '/'
+end
+
